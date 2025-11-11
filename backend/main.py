9a30179876782651c921ff1f8b76371796b5869e
aymen merad <ay28mene@gmail.com>
2025-11-11 23:42:17 +0100

    backend example

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import random
+from pydantic import BaseModel
+
+class RandomInt(BaseModel):
+    value:int
+
+class Comment(BaseModel):
+    name:str
+    comment:str
+    rating:int
+app = FastAPI()
+
+
+@app.get('/')
+def root():
+    return {'hello':'world'}
+
+@app.get('/random')
+def random_val()->RandomInt:
+    i = random.randint(0,255)
+    r = RandomInt(value=i)
+    return r
+
+@app.post('/comment')
+def comment_fun(comment:Comment)->None:
+    print(f'name: {comment.name}, \t comment: {comment.comment}, \t {comment.rating}')
+    return
